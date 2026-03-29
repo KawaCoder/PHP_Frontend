8263ffe5e27996e8914f9be7e150be423a3988ee
KawaCoder <67145585+KawaCoder@users.noreply.github.com>
2026-03-29 18:41:10 +0200

    erreur cheloue

--- controllers/Commentaire/Commentaire.php
+++ controllers/Commentaire/Commentaire.php
@@ -8,9 +8,10 @@ class CommentaireDTO
     public $commentaire;
     public $id_joueur;
 
-    public function __construct(array $data) {
+    public function __construct(array $data)
+    {
         $this->id_commentaire = $data['id_commentaire'];
-        $this->date_commentaire = $data['date_commentaire']';
+        $this->date_commentaire = $data['date_commentaire'];
         $this->commentaire = $data['commentaire'];
         $this->id_joueur = $data['id_joueur'];
     }
